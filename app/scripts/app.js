5d443a89baec192bdd5d270d85315c155a45e1a3
huks <huks7417@gmail.com>
2017-12-03 22:20:40 +0900

    wip: app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,4 @@
+/* Google Maps JavaScript */
 var map;
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
@@ -5,7 +6,16 @@ function initMap() {
     center: {lat: 40.7413549, lng: -73.9980244},
     zoom: 13
     });
+
+    var tribeca = {lat: 40.719526, lng: -74.0089934};
+    var marker = new google.maps.Marker({
+      position: tribeca,
+      map: map,
+      title: 'First Marker!'
+    });
 }
+
+/* Knockout below */
 
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
