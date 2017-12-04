8c3508add7037c4284143baf1c791f8eb247776e
huks <huks7417@gmail.com>
2017-12-04 16:17:16 +0900

    feat: Live search with knockout.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,35 @@
-/* Google Maps JavaScript */
-var map;
-function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: 40.7413549, lng: -73.9980244},
-    zoom: 13
-    });
+/**
+ * Knockout below
+ * Cited from Live search with knockout.js
+ * https://opensoul.org/2011/06/23/live-search-with-knockoutjs/
+ */
+$(function() {
+    // The location listings data - would come from the server
+    var locations = [
+        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+        {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+        {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+        {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+        {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+    ];
 
-    var tribeca = {lat: 40.719526, lng: -74.0089934};
-    var marker = new google.maps.Marker({
-      position: tribeca,
-      map: map,
-      title: 'First Marker!'
-    });
-}
+    var viewModel = {
+        locations: ko.observableArray(locations),
 
-/* Knockout below */
+        query: ko.observable(''),
 
-// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-function AppViewModel() {
-    this.firstName = "Bert";
-    this.lastName = "Bertington";
-}
+        search: function(value) {
+            viewModel.locations.removeAll();
+            for(var x in locations) {
+                if(locations[x].title.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+                    viewModel.locations.push(locations[x]);
+                }
+            }
+        }
+    };
 
-// Activates knockout.js
-ko.applyBindings(new AppViewModel());
+    viewModel.query.subscribe(viewModel.search);
+
+    ko.applyBindings(viewModel);
+});
