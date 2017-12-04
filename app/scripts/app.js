38a25da6786a9fb8a05bc3485c0e813f5fd57165
huks <huks7417@gmail.com>
2017-12-04 17:41:45 +0900

    feat: Live Search with Knockout using Computed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,33 +3,29 @@
  * Cited from Live search with knockout.js
  * https://opensoul.org/2011/06/23/live-search-with-knockoutjs/
  */
-$(function() {
-    // The location listings data - would come from the server
-    var locations = [
-        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-        {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-        {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-        {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-        {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-    ];
 
-    var viewModel = {
-        locations: ko.observableArray(locations),
+// The location listings data - would come from the server
+var locations = [
+    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+    {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+    {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+];
 
-        query: ko.observable(''),
-
-        search: function(value) {
-            viewModel.locations.removeAll();
-            for(var x in locations) {
-                if(locations[x].title.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-                    viewModel.locations.push(locations[x]);
-                }
+function ViewModel() {
+    var self = this;
+    this.filter = ko.observable();
+    this.locations = ko.observableArray(locations);
+    
+    this.filteredLocations = ko.computed(function() {
+        return this.locations().filter(function(location) {
+            if(!self.filter() || location.title.toLowerCase().indexOf(self.filter().toLowerCase()) !== -1) {
+                return location;
             }
-        }
-    };
+        });
+    }, this);
+}
 
-    viewModel.query.subscribe(viewModel.search);
-
-    ko.applyBindings(viewModel);
-});
+ko.applyBindings(new ViewModel());
