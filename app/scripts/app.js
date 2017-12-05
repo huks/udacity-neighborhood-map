ae46fcad04d7ebf80e0f53c704ebb77745b825a8
huks <huks7417@gmail.com>
2017-12-05 11:47:26 +0900

    feat: Add functionality to animate a map marker

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,8 @@
-/* Google Maps JavaScript */
-var map;
-// The location listings data - would come from the server
-var locations = [
+// Creates a global map variable
+var MAP;
+
+// The default location listings data - would come from the server
+var LOCATION_DATA = [
     {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
     {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
     {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
@@ -11,38 +12,56 @@ var locations = [
 ];
 
 /**
- * @description Knockout ViewModel
- * Filter feature cited from https://stackoverflow.com/questions/34584181/create-live-search-with-knockout
+ * @description Represent a single location item
+ * @param location
  */
 function Location(location) {
+    var self = this;
     this.title = ko.observable(location.title);
-    this.location = ko.observable(location.location);
-    console.log(this.title(), this.location());
-    this.visible = ko.observable(true);
+    self.location = ko.observable(location.location);
 
+    // Marker feature cited from...
+
+    // Create a marker for each location
     this.marker = new google.maps.Marker({
-        position: this.location(),
-        title: this.title()
+        position: self.location(),
+        title: this.title(),
+        animation: google.maps.Animation.DROP
     });
-    
-    this.marker.setMap(map);
-}
 
+    // Create an onclick event per marker
+    this.marker.addListener('click', function() {
+        // Animate when the map marker itself is selected
+        MAP.panTo(self.location());
+    })
+
+    // Extend the boundaries of the map for each marker
+    MAP.bounds.extend(this.marker.position);
+    // and display the marker
+    this.marker.setMap(MAP);    
+}
+/**
+ * @description AppViewModel
+ */
 function AppViewModel() {
     var self = this;
 
     // initMap()
-    map = new google.maps.Map(document.getElementById("map"), {
+    MAP = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 40.7413549, lng: -73.9980244},
         zoom: 13,
         mapTypeControl: false
     });
 
+    // initialize bounds variable
+    MAP.bounds = new google.maps.LatLngBounds();
+
     this.filter = ko.observable();
-    this.locations = ko.observableArray(locations.map(function(location) {
+    this.locations = ko.observableArray(LOCATION_DATA.map(function(location) {
         return new Location(location);
     }));
     
+    // Filter feature cited from https://stackoverflow.com/questions/34584181/create-live-search-with-knockout
     this.filteredLocations = ko.computed(function() {
         return this.locations().filter(function(location) {
             var display = true;
@@ -50,11 +69,17 @@ function AppViewModel() {
                 display = true;
             } else {
                 display = false;
-            }
+            }            
             location.marker.setVisible(display);
             return display;
         });
     }, this);
+
+    this.animateMarker = function(param) {
+        MAP.panTo(param.location());
+    }
+
+    MAP.fitBounds(MAP.bounds);
 }
 
 function startApp() {
